34bd4a3a07f6ec046c264868bbfa31eafbd57785
Shawn-Hanclock <nwahs007@gmail.com>
2022-05-05 08:20:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/april11/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.april11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int numDucks = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void upClick (View v)
+    {
+        Toast.makeText(this, "You clicked the up button", Toast.LENGTH_SHORT).show();
+        numDucks++;
+        TextView countTextView = (TextView) findViewById(R.id.countLabelView);
+        countTextView.setText("Number of EagleDucks: " + numDucks);
+        //Log.i("info", "DOES THE INFO WORK????!!");
+    }//end of up counting button handler
+
+    public void downClick (View v)
+    {
+        Toast.makeText(this, "You clicked the down button", Toast.LENGTH_SHORT).show();
+        numDucks--;
+        TextView countTextView = (TextView) findViewById(R.id.countLabelView);
+        countTextView.setText("Number of EagleDucks: " + numDucks);
+        //Log.i("info", "yes");
+    }//end of down counting button handler
+    public void openDialog (View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Confirm reset count");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "YESSIRREE BOB", Toast.LENGTH_SHORT).show();
+                numDucks = 0;
+                //need to update text again
+                updateTextView();
+            }//end onClick overriden method
+        });//end set positive or "Yes" button handler
+
+        builder.setNegativeButton("No shot bro", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //method is empty because all it needs to do is close the dialog
+            }//end on click
+        });//end  set negative button
+
+        AlertDialog resetDialog = builder.create();
+        resetDialog.show();
+    }//end if reset dialog handler
+
+    public void updateTextView()
+    {
+        TextView countTextView = (TextView) findViewById(R.id.countLabelView);
+        countTextView.setText("Number of EaglesDucks: " + numDucks);
+    }
+
+}//end of mainActivity class
