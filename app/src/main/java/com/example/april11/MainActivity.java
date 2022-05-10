2d7f653fcf918dff4a601c1793c13f4ca7dd440b
Shawn-Hanclock <nwahs007@gmail.com>
2022-05-10 08:26:43 -0700

    Added a second screen with navigation buttons to move between screens.
    Updated Activity labels by using strings.xml. This will remove "Hardcoded" warning for those elements.

--- app/src/main/java/com/example/april11/MainActivity.java
+++ app/src/main/java/com/example/april11/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureNextButton();
     }
 
     public void upClick (View v)
@@ -66,4 +69,14 @@ public class MainActivity extends AppCompatActivity {
         countTextView.setText("Number of EaglesDucks: " + numDucks);
     }
 
+    private void configureNextButton()
+    {
+        Button nextButton = (Button) findViewById(R.id.scrn1NextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+    }//end next button handler
 }//end of mainActivity class
